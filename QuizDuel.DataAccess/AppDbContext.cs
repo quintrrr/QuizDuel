4e487ca1fbb46bd49564c27fecabbe98c7ce8f98
arinagomza <a11r221nir3@gmail.com>
2025-05-21 16:13:57 +0300

    Add Model Configurations

--- QuizDuel.DataAccess/AppDbContext.cs
+++ QuizDuel.DataAccess/AppDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using QuizDuel.DataAccess.Configurations;
 using QuizDuel.DataAccess.Models;
 
 namespace QuizDuel.DataAccess
@@ -12,5 +13,11 @@ namespace QuizDuel.DataAccess
     {
         public DbSet<User> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
