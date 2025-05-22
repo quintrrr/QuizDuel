b52aa389540c5ce965a61e5fd18e686b1dd83170
arinagomza <a11r221nir3@gmail.com>
2025-05-22 14:56:30 +0300

    feat(Core): добавлен IPasswordService

---
+++ QuizDuel.Core/Services/IPasswordServise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizDuel.Core.Services
+{
+    public interface IPasswordServise
+    {
+        string GenerateSalt();
+
+        string HashPassword(string password, string salt);
+    }
+}
