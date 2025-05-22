ddd8eba5b799f6a8b5aa22140faafb18b6a0f185
arinagomza <a11r221nir3@gmail.com>
2025-05-22 15:35:18 +0300

    feat(Core): добавлен IAuthService

---
+++ QuizDuel.Core/Services/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuizDuel.Core.DTO;
+
+namespace QuizDuel.Core.Services
+{
+    public interface IAuthService
+    {
+        Task RegisterAsync(RegisterDTO registerDTO);
+        Task LoginAsync(LoginDTO loginDTO);
+    }
+}
