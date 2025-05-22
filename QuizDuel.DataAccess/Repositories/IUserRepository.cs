81d51e1164badfe29608f8b79568b63895302824
arinagomza <a11r221nir3@gmail.com>
2025-05-22 14:04:53 +0300

    code clean up: удалены неиспользуемые usings

--- QuizDuel.DataAccess/Repositories/IUserRepository.cs
+++ QuizDuel.DataAccess/Repositories/IUserRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using QuizDuel.DataAccess.Models;
+﻿using QuizDuel.DataAccess.Models;
 
 namespace QuizDuel.DataAccess.Repositories
 { 
--- QuizDuel.UI/Form1.cs
+++ QuizDuel.UI/Form1.cs
@@ -1,4 +1,3 @@
-using System.Threading.Tasks;
 using QuizDuel.DataAccess;
 using QuizDuel.DataAccess.Repositories;
 
